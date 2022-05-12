807c09f2bd087eb8e0d13e8b93208257d2b33b0c
Beksultan <albinblack7@gmail.com>
2022-05-12 13:43:46 +0300

    Initial Commit

---
+++ TrafficLightsAppSwiftUI/TrafficLightsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsAppSwiftUIApp.swift
+//  TrafficLightsAppSwiftUI
+//
+//  Created by Masaie on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
